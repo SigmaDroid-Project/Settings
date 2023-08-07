d9755dd0582a7edb1697059701acb18691a7c3d7
LurdRaiden <everethduncan@gmail.com>
2023-08-07 15:36:04 -0400

    AboutPhone: Replace sigma built date and release type
    Replace build date and release type with battery and storage

--- src/com/android/settings/deviceinfo/firmwareversion/SigmaInfoPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/SigmaInfoPreferenceController.java
@@ -23,8 +23,10 @@ import android.os.Build;
 import android.os.SystemProperties;
 import android.widget.TextView;
 
+import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 
+import com.sigma.settings.utils.SpecUtils;
 import com.android.settings.R;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.widget.LayoutPreference;
@@ -39,6 +41,15 @@ public class SigmaInfoPreferenceController extends AbstractPreferenceController 
     private static final String PROP_SIGMA_RELEASETYPE = "ro.sigma.release.type";
     private static final String PROP_SIGMA_BUILD_PACKAGE = "ro.sigma.build.package";
     private static final String PROP_SIGMA_MAINTAINER = "ro.sigma.maintainer";
+    private static final String KEY_BUILD_STATUS = "rom_build_status";
+
+    private static final String KEY_STORAGE = "storage";
+    private static final String KEY_CHIPSET = "chipset";
+    private static final String KEY_BATTERY = "battery";
+    private static final String KEY_DISPLAY = "display";
+
+    final String isOfficial = SystemProperties.get(PROP_SIGMA_RELEASETYPE,
+                this.mContext.getString(R.string.device_info_default));
 
     public SigmaInfoPreferenceController(Context context) {
         super(context);
@@ -76,22 +87,58 @@ public class SigmaInfoPreferenceController extends AbstractPreferenceController 
         return SigmaMaintainer;
     }
 
+    private boolean isOfficial() {
+        final String releaseType = SystemProperties.get(PROP_SIGMA_RELEASETYPE, "");
+        return releaseType.length() > 0 &&
+                "official".equals(releaseType.toLowerCase());
+    }
+
+    private String getSigmaBuildStatus() {
+        if (isOfficial()) {
+            return mContext.getString(R.string.build_status_official);
+        }
+        return "";
+    }
+
     @Override
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
         final LayoutPreference SigmaInfoPreference = screen.findPreference(KEY_SIGMA_INFO);
         final TextView version = (TextView) SigmaInfoPreference.findViewById(R.id.version_message);
-        final TextView buildDate = (TextView) SigmaInfoPreference.findViewById(R.id.build_date_message);
-        final TextView releaseType = (TextView) SigmaInfoPreference.findViewById(R.id.release_type_message);
+        final Preference buildStatusPref = screen.findPreference(KEY_BUILD_STATUS);
+        //final TextView buildDate = (TextView) SigmaInfoPreference.findViewById(R.id.build_date_message);
+        //final TextView releaseType = (TextView) SigmaInfoPreference.findViewById(R.id.release_type_message);
         final TextView maintainer = (TextView) SigmaInfoPreference.findViewById(R.id.maintainer_message);
+
+        //final TextView chipText = (TextView) SigmaInfoPreference.findViewById(R.id.chipset_summary);
+        final TextView storText = (TextView) SigmaInfoPreference.findViewById(R.id.cust_storage_summary);
+        final TextView battText = (TextView) SigmaInfoPreference.findViewById(R.id.cust_battery_summary);
+        //final TextView dispText = (TextView) SigmaInfoPreference.findViewById(R.id.cust_display_summary);
+
         final String SigmaVersion = getSigmaVersion();
         final String SigmaBuildDate = getSigmaBuildDate();
         final String SigmaReleaseType = getSigmaReleaseType();
         final String SigmaMaintainer = getSigmaMaintainer();
+        final String buildStatus = getSigmaBuildStatus();
+
+        buildStatusPref.setTitle(buildStatus);
+	    buildStatusPref.setSummary(SigmaMaintainer);
+
         version.setText(SigmaVersion);
-        buildDate.setText(SigmaBuildDate);
-        releaseType.setText(SigmaReleaseType);
+        //buildDate.setText(SigmaBuildDate);
+        //releaseType.setText(SigmaReleaseType);
         maintainer.setText(SigmaMaintainer);
+
+        //chipText.setText(SpecUtils.getProcessorModel());
+        storText.setText(String.valueOf(SpecUtils.getTotalInternalMemorySize()) + "GB ROM + " + SpecUtils.getTotalRAM() + " RAM");
+        battText.setText(SpecUtils.getBatteryCapacity(mContext) + " mAh");
+        //dispText.setText(SpecUtils.getScreenRes(mContext)); 
+
+        if (isOfficial.toLowerCase().contains("official")) {
+		 buildStatusPref.setIcon(R.drawable.verified);
+	    } else {
+		buildStatusPref.setIcon(R.drawable.unverified);
+	    }
     }
 
     @Override
