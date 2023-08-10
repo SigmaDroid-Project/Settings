SigmaDroid-Project/Settings

commit 5a1f32eb89a1be88af45816246fc0732da6aa65c

AboutPhone: Reorder some properties shown in about phone page

src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -171,14 +171,12 @@ public class MyDeviceInfoFragment extends DashboardFragment
                 mPreference.setLayoutResource(R.layout.sigma_card_build_status);
                 } else if (
                     mKey.equals("rom_build_status")
-                        || mKey.equals("wifi_ip_address")
                         || mKey.equals("firmware_version")
                         || mKey.equals("sim_status")
                 ) {
                     mPreference.setLayoutResource(R.layout.dot_top_no_chevron);
                 } else if (
                     mKey.equals("selinux_status")
-                        || mKey.equals("radio_info_settings")
                         || mKey.equals("bt_address")
                         || mKey.equals("sleep_time")
                 ) {

res/xml/my_device_info.xml
@@ -19,13 +19,12 @@
     xmlns:android="http://schemas.android.com/apk/res/android"
     xmlns:settings="http://schemas.android.com/apk/res-auto"
     android:key="my_device_info_pref_screen"
-    android:title="@string/about_settings">
+    android:title="">
 
-    <com.android.settingslib.widget.LayoutPreference
-        android:key="my_device_info_header"
-        android:order="-1"
-        android:layout="@layout/settings_entity_header"
-        android:selectable="false"/>
+        <!-- SigmaDroid logo -->
+        <PreferenceScreen
+            android:key="sigma_logo"
+            android:selectable="false" />
 
         <!-- SigmaDroid info -->
         <com.android.settingslib.widget.LayoutPreference
@@ -66,10 +65,20 @@
             settings:enableCopying="true"
             settings:controller="com.android.settings.deviceinfo.BuildNumberPreferenceController"/>
 
+        <!-- Baseband -->
+        <Preference
+            android:key="base_band"
+            android:order="7"
+            android:title="@string/baseband_version"
+            android:summary="@string/summary_placeholder"
+            android:icon="@drawable/ic_android"
+            settings:enableCopying="true"
+            settings:controller="com.android.settings.deviceinfo.firmwareversion.BasebandVersionPreferenceController"/>
+
         <!-- Kernel -->
         <com.android.settings.deviceinfo.firmwareversion.KernelVersionPreference
             android:key="kernel_version"
-            android:order="7"
+            android:order="8"
             android:title="@string/kernel_version"
             android:summary="@string/summary_placeholder"
             android:icon="@drawable/ic_kernel"
@@ -79,7 +88,7 @@
         <!-- SELinux -->
         <Preference
             android:key="selinux_status"
-            android:order="8"
+            android:order="9"
             android:title="@string/selinux_status"
             android:summary="@string/summary_placeholder"
             android:icon="@drawable/ic_selinux"
@@ -89,7 +98,7 @@
         <!-- Android version -->
         <Preference
             android:key="firmware_version"
-            android:order="9"
+            android:order="10"
             android:icon="@drawable/ic_android_head"
             android:title="@string/firmware_version"
             android:summary="@string/summary_placeholder"
@@ -99,7 +108,7 @@
         <!-- Account name -->
         <Preference
             android:key="branded_account"
-            android:order="10"
+            android:order="11"
             android:icon="@drawable/ic_cog"
             android:title="@string/my_device_info_account_preference_title"
             android:summary="@string/summary_placeholder"
@@ -109,9 +118,9 @@
         <!-- Phone number -->
         <com.android.settings.deviceinfo.PhoneNumberSummaryPreference
             android:key="phone_number"
+            android:order="12"
             android:title="@string/status_number"
             android:icon="@drawable/phone_tint"
-            android:order="11"
             android:summary="@string/summary_placeholder"
             settings:isPreferenceVisible="@bool/config_show_sim_info"
             settings:controller="com.android.settings.deviceinfo.PhoneNumberPreferenceController"
@@ -120,7 +129,7 @@
         <!-- Security patch -->
         <Preference
             android:key="security_key"
-            android:order="13"
+            android:order="14"
             android:title="@string/security_patch"
             android:icon="@drawable/ic_security_patch"
             settings:enableCopying="true"
@@ -129,7 +138,7 @@
        <!-- Device up time -->
         <Preference
             android:key="up_time"
-            android:order="14"
+            android:order="15"
             android:icon="@drawable/ic_cog"
             android:title="@string/status_up_time"
             android:summary="@string/summary_placeholder" />
@@ -137,19 +146,19 @@
         <!-- Device deep sleep time -->
         <Preference
             android:key="sleep_time"
-            android:order="15"
+            android:order="16"
             android:icon="@drawable/ic_sleep"
             android:title="@string/status_sleep_time"
             android:summary="@string/summary_placeholder" />
 
         <Preference
-            android:order="16"
+            android:order="17"
             android:title="@string/my_device_info_device_identifiers_category_title"/>
         
         <!-- SIM status -->
         <Preference
             android:key="sim_status"
-            android:order="19"
+            android:order="18"
             android:icon="@drawable/ic_sim_card"
             android:title="@string/sim_status_title"
             settings:keywords="@string/keywords_sim_status"

commit 629d8b6c3921c0e8469600c63827d945928beab9

AboutPhone: Remove my_devide_info_header
This should fix force close caused by commit 5a1f32eb89a1be88af45816246fc0732da6aa65c

src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -113,12 +113,6 @@ public class MyDeviceInfoFragment extends DashboardFragment
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        initHeader();
-    }
-
-    @Override
     public void onPause() {
         super.onPause();
         Context context = getContext();
@@ -235,40 +229,6 @@ public class MyDeviceInfoFragment extends DashboardFragment
             return;
         }
         super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    private void initHeader() {
-        // TODO: Migrate into its own controller.
-        final LayoutPreference headerPreference =
-                getPreferenceScreen().findPreference(KEY_MY_DEVICE_INFO_HEADER);
-        final boolean shouldDisplayHeader = getContext().getResources().getBoolean(
-                R.bool.config_show_device_header_in_device_info);
-        headerPreference.setVisible(shouldDisplayHeader);
-        if (!shouldDisplayHeader) {
-            return;
-        }
-        final View headerView = headerPreference.findViewById(R.id.entity_header);
-        final Activity context = getActivity();
-        final Bundle bundle = getArguments();
-        final EntityHeaderController controller = EntityHeaderController
-                .newInstance(context, this, headerView)
-                .setRecyclerView(getListView(), getSettingsLifecycle())
-                .setButtonActions(EntityHeaderController.ActionType.ACTION_NONE,
-                        EntityHeaderController.ActionType.ACTION_NONE);
-
-        // TODO: There may be an avatar setting action we can use here.
-        final int iconId = bundle.getInt("icon_id", 0);
-        if (iconId == 0) {
-            final UserManager userManager = (UserManager) getActivity().getSystemService(
-                    Context.USER_SERVICE);
-            final UserInfo info = Utils.getExistingUser(userManager,
-                    android.os.Process.myUserHandle());
-            controller.setLabel(info.name);
-            controller.setIcon(
-                    com.android.settingslib.Utils.getUserIcon(getActivity(), userManager, info));
-        }
-
-        controller.done(context, true /* rebindActions */);
     }
 
     @Override