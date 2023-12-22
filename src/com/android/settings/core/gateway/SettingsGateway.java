03ffe73e02a367a1028c74cd7ee9c9779684c7aa
elpaablo <donopwhile1@gmail.com>
2023-12-22 16:29:36 +0000

    Update a few classes location, after reorganizing Alpha settings

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -196,7 +196,7 @@ import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSetting
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.alpha.settings.AlphaSettings;
-import com.alpha.settings.fragments.ui.SmartPixels;
+import com.alpha.settings.fragments.misc.SmartPixels;
 
 import ink.kscope.settings.wifi.tether.WifiTetherClientManager;
 
