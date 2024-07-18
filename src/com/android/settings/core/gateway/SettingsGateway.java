d69224195105797df8f4ec66f130f953a7532a93
Matt Filetto <matt.filetto@gmail.com>
2024-07-18 08:55:26 -0700

    [SQUASH] Revert MatrixxSettings changes
    Revert "Settings: Improve about page design"
    
    This reverts commit 7320b78f49e1a4a89ec6b981031462bd912cd028.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    
    Revert "Settings: Make Settings homepage widget compact"
    
    This reverts commit 1429942e86e28cede815e71dfc427ee7a5bb558f.
    
    Revert "Settings: Use solo card for Matrixx Settings"
    
    This reverts commit 848c8ed28fbf0d4f6b318fe78d77991c298c08d7.
    
    Revert "Settings: Solo card style theming changes"
    
    This reverts commit 92d951ba9ae0c914070131515759a7e466eb2deb.
    
    Revert "Settings: Redesign about phone ui"
    
    This reverts commit 2d243f43e2f71fc095d5179ab5558ab72977ea23.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    
    Revert "Settings: About Phone: Cleanup & Improvements"
    
    This reverts commit 03616726bcb2a1abcaa60fb7d234e93b7b3766bc.
    
    Revert "Settings: Nuke memory info from About phone"
    
    This reverts commit 7b11ca270d06bdae88c1c4b281f484e7960a5f2a.
    
    Revert "Settings: Implement simple/no widgets homepage ui"
    
    This reverts commit a1f9691fa8258b1fa4fe7cc5ee98da25e2b28cf8.
    
    Revert "Settings: Import Setting's homeapge & About phone ui from Rising"
    
    This reverts commit 1f9d4f34b68684fa546d47da06ea93fc732d498b.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    
    Revert "Settings: Rearrange Display of ROM version/type/variant"
    
    This reverts commit f62ac7db3e418fdf492ee5d07913a34824e8ffeb.
    
    Revert "Settings: add maintainer info to firmware page"
    
    This reverts commit a52f15ab3e673bf4ff0857f79ad20228712537ed.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    
    Revert "Adapt firmware version to Matrixx"
    
    This reverts commit fcf986030d14a37adcbd1a4cb4facf0b33b564ab.
    
    Revert "Settings: Initial Matrixx conversion"
    
    This reverts commit e0eb16d806964fd7e36ebdcd7ee378e144d27a84.
    
    Revert the following commits:
    
    Revert "Revert "Changelog: Rework with Activity [1/2]""
    
    This reverts commit 214d4b9fd232c8655e8ba5d4dc7a6da18fa135cb.
    
    Revert "Settings: Improve about page design"
    
    This reverts commit d24234e87a1e65fbcbcfde76e58b44a1ea469081.
    
    Revert "Settings: Make Settings homepage widget compact"
    
    This reverts commit 706975ca1e2f765b8ece16cbe23444bb8f2d9013.
    
    Revert "Settings: Use solo card for Matrixx Settings"
    
    This reverts commit 4c62fb5a2f046e3c1f5e023ff281b15efe96e701.
    
    Revert "Settings: Solo card style theming changes"
    
    This reverts commit 56ba3bf3c47a1163fb366385cf77ea667214a79a.
    
    Revert "Settings: Redesign about phone ui"
    
    This reverts commit 2771b2c7044f8d779688a789e33b4a59d3e43614.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    
    Revert "Settings: About Phone: Cleanup & Improvements"
    
    This reverts commit 2830b0fcc46c9a21b886297cc464b05cbd4ec903.
    
    Revert "Settings: Nuke memory info from About phone"
    
    This reverts commit ff11a21ac4030ba6202799608c626e41ca562008.
    
    Revert "Settings: Implement simple/no widgets homepage ui"
    
    This reverts commit 87e6eaf98257b58fd42ab56f08c608ed2b565d0e.
    
    Revert "Settings: Import Setting's homeapge & About phone ui from Rising"
    
    This reverts commit 29f2c55441b7ddce4e12ac32326efc1af9f974c4.
    
    Revert "Settings: Rearrange Display of ROM version/type/variant"
    
    This reverts commit 42557979d2139844dc939f093c5075900407b287.
    
    Revert "Settings: add maintainer info to firmware page"
    
    This reverts commit 8bd90550cf0d5341e4757afe1e991469865b1581.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    
    Revert "Adapt firmware version to Matrixx"
    
    This reverts commit cabd638016a9716c637ccd7c5385d1f7d76d75b2.
    
    Revert "Settings: Initial Matrixx conversion"
    
    This reverts commit b058b9a184876c3d192fb50427106ae4b90a4ae4.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -449,7 +449,5 @@ public class SettingsGateway {
             Settings.DevRunningServicesActivity.class.getName(),
             Settings.SleepModeActivity.class.getName(),
             Settings.SmartPixelsActivity.class.getName(),
-            Settings.FirmwareVersionActivity.class.getName(),
-            Settings.WallpaperSettingsActivity.class.getName(),
     };
 }
